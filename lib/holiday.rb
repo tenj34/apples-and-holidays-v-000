a9b4af892b847f7192ca350b5bb80503f70b27a2
Juan Tenezaca <tenj34@gmail.com>
2018-07-28 18:02:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -51,7 +51,6 @@ def all_winter_holiday_supplies(holiday_hash)
     supply_array
     end.flatten
   end
-end
 
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
